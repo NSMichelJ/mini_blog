ad34843cbacce3ddd401fa71dd7aebbee828024a
NSMichelJ <nunezsalmeronm@gmail.com>
2021-12-22 14:38:49 -0400

    Agregado endpoints show_users y show_posts, en el modulo admin

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,7 +1,10 @@
 from flask import Blueprint
 from flask import render_template
+from flask import request
 from flask_login import login_required
 
+from app.auth.models import User
+from app.post.models import Post
 from .decorators import admin_required
 
 bp = Blueprint('admin', __name__, template_folder='templates')
@@ -11,3 +14,17 @@ bp = Blueprint('admin', __name__, template_folder='templates')
 @admin_required
 def admin():
     return render_template('admin.html')
+
+@bp.route('/admin/users')
+@login_required
+@admin_required
+def show_users():
+    users = User.query.all()
+    return render_template('show_users.html', users=users)
+
+@bp.route('/admin/posts')
+@login_required
+@admin_required
+def show_posts():
+    posts = Post.query.all()
+    return render_template('show_posts.html', posts=posts)
