8670c4973d534f7ab66315e6451da635b06c0f95
NSMichelJ <nunezsalmeronm@gmail.com>
2021-12-26 09:15:31 -0400

    Agregado endpoint para buscar publicaciones

--- app/public/routes.py
+++ app/public/routes.py
@@ -1,13 +1,22 @@
 from flask import Blueprint
 from flask import render_template
+from flask import request
 
 from app.auth.models import User
+from app.post.models import Post
+from app.ext import db
 
 bp = Blueprint('public', __name__, template_folder='templates')
 
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+@bp.route('/search')
+def search():
+    q = request.args.get('q', '')
+    results = Post.query.filter(Post.title.contains(q)).all()
+    return render_template('search.html', results=results)
 
 @bp.route('/user/<string:username>')
 def show_user(username):
